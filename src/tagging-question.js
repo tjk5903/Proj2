0eade34e3c0213c2cabbb9a48c49f7624a82b24e
tjk5903 <tjk5903@psu.edu>
2024-04-21 12:37:22 -0400

    picture added

--- src/tagging-question.js
+++ src/tagging-question.js
@@ -102,7 +102,7 @@ export class TaggingQuestion extends LitElement {
     this.droppedTags = [];
     this.isAnswered = false;
     this.feedbackMessage = '';
-    this.imageData = ''; 
+    this.imageData = 'https://t3.ftcdn.net/jpg/02/43/25/90/360_F_243259090_crbVsAqKF3PC2jk2eKiUwZHBPH8Q6y9Y.jpg'; 
     this.question = '';
   }
 
